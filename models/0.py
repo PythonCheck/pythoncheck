86c7bae4fe088523ea6f7c180f2b4cf4f5336d9e
System Administrator <root@Jonass-MacBook-Air.local>
2012-11-12 11:14:45 +0100

    first commit

---
+++ models/0.py
@@ -0,0 +1,19 @@
+from gluon.storage import Storage
+settings = Storage()
+
+settings.migrate = True
+settings.title = 'Python Check'
+settings.subtitle = ''
+settings.author = ''
+settings.author_email = ''
+settings.keywords = ''
+settings.description = ''
+settings.layout_theme = 'Customize'
+settings.database_uri = 'sqlite://storage.sqlite'
+settings.security_key = '623ce349-7e66-4d5f-bdc3-ac72e2af9773'
+settings.email_server = ''
+settings.email_sender = ''
+settings.email_login = ''
+settings.login_method = 'local'
+settings.login_config = ''
+settings.plugins = []
