b1982e890156db1774572cea1a2554020b937c53
Daniel Laxar <i.dlaxar@gmail.com>
2012-11-14 21:10:29 +0100

    adding new jail scripts

--- scripts/jail/create.sh
+++ scripts/jail/create.sh
@@ -1,16 +1,28 @@
 ROOT=/root/jail
-INCPATH=/lib64/
-includes=( "/lib64/libtinfo.so.5" /lib64/libdl.so.2 /lib64/librt.so.1 /lib64/libcap.so.2 /lib64/libacl.so.1 /lib64/libc.so.6 /lib64/libdl.so.2 /lib64/ld-linux-x86-64.so.2 )
 
-mkdir -p $ROOT/lib64 $ROOT/bin
-cp /bin/bash /bin/ls $ROOT/bin/
+files=( /lib64/libtinfo.so.5 /lib64/libdl.so.2 /lib64/librt.so.1 /lib64/libcap.so.2 /lib64/libacl.so.1 /lib64/libc.so.6 
+	/lib64/libdl.so.2 /lib64/ld-linux-x86-64.so.2 /usr/lib64/python26.zip /usr/lib64/python2.6 /usr/lib64/python2.6/plat-linux2 
+	/usr/lib64/python2.6/lib-tk /usr/lib64/python2.6/lib-old /usr/lib64/python2.6/lib-dynload /usr/lib64/python2.6/site-packages 
+	/usr/lib/python2.6/site-packages /usr/lib64/libpython2.6.so.1.0 /lib64/libpthread.so.0 /lib64/libdl.so.2 /lib64/libutil.so.1 /lib64/libm.so.6 
+)
+
+# create 
+mkdir -p $ROOT $ROOT
 
 # copy any dynamically linked libraries
-for inc in ${includes[@]}
+for index in ${files[@]} 
 do
-	cp $inc $ROOT$INCPATH
-	echo $inc
+	file=$index
+	if [[ -h $index  ]]
+	then
+		f=`readlink -f $index`
+		echo $f
+		file=$index" "$f
+	fi
+	cp --parents -r $file $ROOT
 done
+cp --parent /usr/bin/python /bin/bash /bin/ls $ROOT
+
  
 # finally switch to root
 chroot $ROOT
