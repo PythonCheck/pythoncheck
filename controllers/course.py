d02d27355c5733c9e28a3edf12d3d9e630360dbd
Jonas Keisel <jonas.keisel@gmail.com>
2012-11-24 10:30:37 +0100

    Implemented create new course!

--- controllers/course.py
+++ controllers/course.py
@@ -1,10 +1,14 @@
 
+def validate_new_user(form):
+	form.vars.teacher = auth.user_id
+
 def new():
-	if db(auth.user_id == db.auth_membership.user_id).select()[0].group_id < 2:
+	if auth.user_id is None or db(auth.user_id == db.auth_membership.user_id).select()[0].group_id < 2:
 		raise HTTP(403)
 	form = SQLFORM(db.course, fields=['name'])
-	if form.process().accepted:
-		response.flash = ''
+	if form.accepts(request.vars, session, onvalidation=validate_new_user):
+		response.flash = 'Created course "' + form.vars.name + '"'
+		redirect(URL(list))
 	elif form.errors:
 		response.flash = 'form has errors'
 	return dict(form=form)
