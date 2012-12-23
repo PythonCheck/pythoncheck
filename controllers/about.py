1e7a5da3781934b491149d67d09b396350538dc6
Daniel Laxar <i.dlaxar@gmail.com>
2012-12-23 12:35:23 +0100

    adds basic about page

---
+++ controllers/about.py
@@ -0,0 +1,8 @@
+def index():
+	return dict( \
+		peopleCaption=T('These are the people, that have helped to create this plattform'),
+		people=[dict(name='Jonas Keisel', job=T('Programmer')), \
+				dict(name='Daniel Laxar', job=T('Programmer')), \
+				dict(name='Tobias Primus', job=T('Project Manager')), \
+				dict(name='Armin Redzic', job=T('Layout & Design'))] \
+		)
